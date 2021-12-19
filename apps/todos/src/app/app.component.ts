91d83e42d8a87d6053a1e79e02440d90f4d212a8
RonAlmog <ron8132@gmail.com>
2021-12-19 11:40:41 -0500

    add nest app

--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 interface Todo {
   title: string;
@@ -12,7 +13,16 @@ export class AppComponent {
   title = 'todos';
   todos: Todo[] = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
 
+  constructor(private http: HttpClient) {
+    this.fetch();
+  }
+
+  fetch() {
+    this.http.get<Todo[]>('/api/todos').subscribe((t) => (this.todos = t));
+  }
   addTodo() {
-    this.todos.push({ title: 'random num' });
+    this.http.post('/api/addTodo', {}).subscribe(() => {
+      this.fetch();
+    });
   }
 }
